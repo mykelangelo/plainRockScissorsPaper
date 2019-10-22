e8eaa4fa2dee3ad0ea4b651b765341de0cce78b3
Mykola Papenko <mykola.papenko@gmail.com>
2019-10-23 00:03:16 +0300

    set up hello-world server

---
+++ src/main/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, _ *http.Request) {
+
+	var _, err = io.WriteString(w, "Hello, world!")
+
+	if err != nil {
+		print(err)
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/", hello)
+
+	var err = http.ListenAndServe(":80", nil)
+
+	if err != nil {
+		print(err)
+	}
+}
