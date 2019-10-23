da47f91c25a388997167244a717edfe9aab89a02
Mykola Papenko <mykola.papenko@gmail.com>
2019-10-23 05:56:21 +0300

    retrieve port from env var PORT

--- src/main/main.go
+++ src/main/main.go
@@ -3,40 +3,36 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
 )
 
 func hello(w http.ResponseWriter, _ *http.Request) {
 
-	print("hello-0")
-
 	var _, err = io.WriteString(w, "Hello, YOLO!")
 
-	print("hello-1")
-
 	if err != nil {
-		print("hello-err")
 		print("hello", err.Error())
 	}
-
-	print("hello-end")
 }
 
 func main() {
 
-	print("main-0")
-
 	http.HandleFunc("/", hello)
 
-	print("main-1")
-
-	var err = http.ListenAndServe(":8000", nil)
-
-	print("main-2")
+	var err = http.ListenAndServe(":"+getPort(), nil)
 
 	if err != nil {
-		print("main-err")
 		print("main", err.Error())
 	}
+}
 
-	print("main-end")
+func getPort() string {
+
+	var port = os.Getenv("PORT")
+
+	if len(port) != 4 {
+		port = "8000"
+	}
+
+	return port
 }
