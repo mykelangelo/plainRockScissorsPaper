d2823271fdd59371db183f858afc30957480e763
Mykola Papenko <mykola.papenko@gmail.com>
2019-10-23 12:22:40 +0300

    set port (per cmd window)

--- run.bat
+++ run.bat
@@ -1,5 +1,7 @@
+#run local instance (Windows 10 script only yet)
 cd .\src\github.com\mykelangelo\plainRockScissorsPaper
 cd .\src\main
+set port=80
 set name=plainRockScissorsPaper.exe
 set app=".\..\..\..\..\..\..\bin\%name%"
 taskkill /F /IM %name%
